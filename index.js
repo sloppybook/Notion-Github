8875fbf0f4e693c7dc95aae4cfa12c1bd62fea3d
sakamoto kosei <33928882+sloppybook@users.noreply.github.com>
2022-12-08 22:00:46 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+import {getInput, setFailed} from '@actions/core'
+import {Client, LogLevel} from '@notionhq/client'
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+    logLevel: LogLevel.DEBUG,
+})
+
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID
+const GITHUB_REPOSITORY_URL = 'https://github.com/tommy6073/issue-and-notion-database-page'
+
+const TITLE = 'title'
+const CATEGORY = 'category'
+const ISSUE_NUMBER = 'issue-number'
+
+try {
+    await notion.request({
+        path: 'pages',
+        method: 'post',
+        body: {
+            parent: {database_id: NOTION_DATABASE_ID},
+            properties: {
+                Title: [{text: {content: getInput(TITLE)}}],
+                Category: {name: getInput(CATEGORY)},
+                Link: `${GITHUB_REPOSITORY_URL}/issues/${getInput(ISSUE_NUMBER)}`,
+            },
+        },
+    })
+} catch (error) {
+    setFailed(error.message)
+}
