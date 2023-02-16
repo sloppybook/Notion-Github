9d72aaa1ff91448620771e53d2df71d1d18a0e6e
sloppybook <sloppybook@gmail.com>
2023-02-16 15:57:28 +0900

    fix index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN,
     logLevel: LogLevel.DEBUG,
 })
+
 const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID
-const GITHUB_REPOSITORY_URL = 'https://github.com/sloppybook/Notion-Github'
+const GITHUB_REPOSITORY_URL = process.env.GITHUB_REPOSITORY_URL
 
 const TITLE = 'title'
 const CATEGORY = 'category'
@@ -19,7 +20,9 @@ try {
         body: {
             parent: {database_id: NOTION_DATABASE_ID},
             properties: {
-                title: [{text: {content: getInput(TITLE)}}]
+                title: [{text: {content: getInput(TITLE)}}],
+                Category: {name: getInput(CATEGORY)},
+                Link: `${GITHUB_REPOSITORY_URL}/issues/${getInput(ISSUE_NUMBER)}`,
             },
         },
     })
